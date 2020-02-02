d3b6c890751c4747b6f2abd31e685f70de2ab757
Kaze <Erica_Kaze@baylor.edu>
2020-02-02 16:56:04 -0600

    updated on sunday

---
+++ csv_temps_2.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt 
+
+import csv
+
+open_file= open("sitka_weather_07-2018_simple.csv", "r")
+
+csv_file=csv.reader(open_file, delimiter=",")
+
+header_row= next (csv_file)
+
+print(type(header_row))
+
+for index, column_header in enumerate (header_row):
+    print(index, column_header)
+
+highs =[]
+dates= []
+
+for row in csv_file:
+    highs.append(int(row[5]))
+    dates.append(row[2])
+
+#print(highs[:10])
+#print(test)
+
+fig=plt.figure()
+
+
+plt.plot(dates, highs, color='red')
+plt.title("daily high temps, July2018", fontsize=16)
+plt.xlabel ("", fontsize=12)
+plt.ylabel ("Temperature (F)", fontsize=12)
+plt.tick_params (axis='both', which="major", labelsize=12)
+
+fig.autofmt_xdate()
+
+
+plt.show()
